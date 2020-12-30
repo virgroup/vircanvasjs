1d17b4771e8192f5bbf6853eaf83e3f4f7837014
amincs <amincs@outlook.com>
2020-12-30 22:18:58 +0330

    Create index.js of utils

---
+++ src/lib/utils/index.js
@@ -0,0 +1,14 @@
+import "./vir_is_strict_object";
+import "./vir_in_types";
+import "./vir_validator";
+import "./vir_abstract_class";
+import "./vir_proxy_handler";
+
+
+export {
+    virIsStrictObject,
+    virInTypes,
+    virValidator,
+    VirAbstractClass,
+    virProxyHandler,
+};
