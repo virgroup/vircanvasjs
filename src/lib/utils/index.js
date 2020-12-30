253849fc6dda63d34f22a0b152abd75ec9977da8
amincs <amincs@outlook.com>
2020-12-30 23:22:14 +0330

    Change utils index file

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,8 +1,8 @@
-import "./vir_is_strict_object";
-import "./vir_in_types";
-import "./vir_validator";
-import "./vir_abstract_class";
-import "./vir_proxy_handler";
+import virIsStrictObject from "./vir_is_strict_object";
+import virInTypes from "./vir_in_types";
+import virValidator from "./vir_validator";
+import VirAbstractClass from "./vir_abstract_class";
+import virProxyHandler from "./vir_proxy_handler";
 
 
 export {
