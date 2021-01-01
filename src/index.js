dd43008f492c292e7a5fa4519add9225d9114eb5
amincs <amincs@outlook.com>
2021-01-01 20:44:56 +0330

    Add Line to VirCanvas object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import Canvas from "./lib/canvas";
+import {Line} from "./lib/shapes";
 
 if (typeof window !== 'undefined') {
     var virCanvas = {
-        create: Canvas
+        create: Canvas,
+        Line: Line
     };
 
     window.VirCanvas = virCanvas;
 }
 
-export default Canvas;
+export {
+    Canvas as Default,
+    Line
+};
