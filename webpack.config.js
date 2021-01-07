01c18a42a8720673d8b718b89cc7ff9627cbb6d2
amincs <amincs@outlook.com>
2021-01-07 22:15:06 +0330

    Install eslint

--- webpack.config.js
+++ webpack.config.js
@@ -15,19 +15,17 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
-        rules: [
-        {
+        rules: [{
             test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-class-properties']
-                    }
+            exclude: /(node_modules|bower_components)/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-class-properties']
                 }
-            }
-        ]
+            },'eslint-loader']
+        }]
     },
     mode: "development",
     plugins: [
